2f2cacf624a00df390f818072ca7ab0a7234d3ec
Santiago Acuña <snt-26@hotmail.com>
2020-09-12 01:46:22 -0500

    Finished ViajeIndividual Class

--- AgenciaViajes/src/main/java/co/unicauca/tallerpolimorfismo/modelo/viaje/ViajeIndividual.java
+++ AgenciaViajes/src/main/java/co/unicauca/tallerpolimorfismo/modelo/viaje/ViajeIndividual.java
@@ -1,9 +1,20 @@
 package co.unicauca.tallerpolimorfismo.modelo.viaje;
 
+import java.util.Date;
+
 /**
- * 
+ * Clase para crear un viaje individual que hereda de una clase base
  * @author Santiago Acuña
  */
-public class ViajeIndividual {
+public class ViajeIndividual extends Viaje {
+    
+    public ViajeIndividual(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+    }
+
+    @Override
+    public String descripcion() {
+        return "Disfruta de tu viaje individual";
+    }
 
 }
