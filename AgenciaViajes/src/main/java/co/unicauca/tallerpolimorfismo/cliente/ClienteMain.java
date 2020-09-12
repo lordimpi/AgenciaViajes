e38050c438ef1db3d4d8b427c3974f26bca96538
Santiago Acuña <snt-26@hotmail.com>
2020-09-12 00:33:03 -0500

    commit inicial

---
+++ AgenciaViajes/src/main/java/co/unicauca/tallerpolimorfismo/cliente/ClienteMain.java
@@ -0,0 +1,16 @@
+package co.unicauca.tallerpolimorfismo.cliente;
+
+/**
+ *
+ * @author Santiago Acuña
+ */
+public class ClienteMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
